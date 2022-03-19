31576bbe85b6eb7f751ce4f57be672868a6958b4
rahat664 <rahatkabir20@gmail.com>
2022-03-20 00:11:17 +0600

    feat: navbar and footer

--- aimart/src/app/app-routing.module.ts
+++ aimart/src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'home',
+    loadChildren: () => import('./Pages/home/home.module').then(m => m.HomeModule),
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./Pages/about/about.module').then(m => m.AboutModule),
+    pathMatch: 'full'
+  },
+  {
+    path: 'contact',
+    loadChildren: () => import('./Pages/contact/contact.module').then(m => m.ContactModule),
+    pathMatch: 'full'
+  },
+  {
+    path: 'products',
+    loadChildren: () => import('./Pages/products/products.module').then(m => m.ProductsModule),
+    pathMatch: 'full'
+  },
+  {
+    path: 'cart',
+    loadChildren: () => import('./Pages/cart/cart.module').then(m => m.CartModule),
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
