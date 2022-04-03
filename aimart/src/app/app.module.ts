5a6957eb43cb6293d7d4d7ffcbd0c2c931a153ed
rahat664 <rahatkabir20@gmail.com>
2022-04-03 23:50:40 +0600

    feat: single product page added

--- aimart/src/app/app.module.ts
+++ aimart/src/app/app.module.ts
@@ -6,13 +6,15 @@ import {AppComponent} from './app.component';
 import {SharedModule} from "./shared/shared.module";
 import {SwiperModule} from "swiper/angular";
 import {HttpClientModule} from "@angular/common/http";
-import { CheckoutComponent } from './Pages/checkout/checkout.component';
+import {CheckoutComponent} from './Pages/checkout/checkout.component';
+import {ProductComponent} from './product/product.component';
 
 
 @NgModule({
     declarations: [
         AppComponent,
         CheckoutComponent,
+        ProductComponent,
     ],
     imports: [
         BrowserModule,
